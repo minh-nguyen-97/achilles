340288d165549b0f89042b8ef1485ee76dc9ebda
AchillesS <qmn1312@gmail.com>
2019-09-30 12:07:05 +1000

    Dashboard UI and success message flash

--- src/auth/passport-local-config.js
+++ src/auth/passport-local-config.js
@@ -4,20 +4,21 @@ const LocalStategy = require('passport-local').Strategy;
 module.exports = (passport) => {
   passport.use(new LocalStategy({
       usernameField: 'email',
-      passwordField: 'password'
-    }, async (email, password, done) => {
+      passwordField: 'password',
+      passReqToCallback: true
+    }, async (req, email, password, done) => {
 
       User.findOne({ email }, async (err, user) => {
         if (err) { return done(err); }
         if (!user) {
-          return done(null, false, { message: 'Invalid username or password' });
+          return done(null, false, req.flash('error_flash', 'Invalid username or password'));
         }
 
         const isMatch = await user.verifyPassword(password);
         if (!isMatch) {
-          return done(null, false, { message: 'Invalid username or password' });
+          return done(null, false, req.flash('error_flash', 'Invalid username or password'));
         }
-        return done(null, user);
+        return done(null, user, req.flash('success_flash', `Welcome to Achilles, ${user.username}`));
       });
 
     })
